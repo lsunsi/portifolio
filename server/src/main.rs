4980d28b2681e199b14eb43dbfa1c57d71b14c15
lsunsi <sunsi.lucas@gmail.com>
2020-06-07 13:12:18 -0300

    Add setup for actix and diesel
    The intention here was just to get this working and learn about all
    the wiring needed. Check and check.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::web::Data;
+use actix_web::{get, App, HttpServer};
+use diesel::prelude::PgConnection;
+use diesel::r2d2::{ConnectionManager, Pool};
+
+type Database = Pool<ConnectionManager<PgConnection>>;
+
+#[get("/")]
+async fn index(_: Data<Database>) -> &'static str {
+    "oie"
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    let connspec = std::env::var("DATABASE_URL").expect("DATABASE_URL");
+    let manager = ConnectionManager::<PgConnection>::new(connspec);
+    let pool = Pool::builder().build(manager).unwrap();
+
+    HttpServer::new(move || App::new().data(pool.clone()).service(index))
+        .bind("0.0.0.0:8000")?
+        .run()
+        .await
 }
