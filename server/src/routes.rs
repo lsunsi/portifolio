1467f2b1d5cf21d078160edba375d4108098ecc2
lsunsi <sunsi.lucas@gmail.com>
2020-07-01 18:40:06 -0300

    Add import-etf-prices route with parsing logic and BOVA11 asset
    I mean, sure, it should be generic and support more assets. But I
    thought this was a good enough release so I could expand later.
    Also worries about structure and modularization come later, when I
    feel like it.

--- server/src/routes.rs
+++ server/src/routes.rs
@@ -1,9 +1,11 @@
 mod healthz;
+mod import_etf_prices;
 mod import_treasury_prices;
 
 use actix_web::web::ServiceConfig;
 
 pub fn config(cfg: &mut ServiceConfig) {
     cfg.service(healthz::healthz)
-        .service(import_treasury_prices::post);
+        .service(import_treasury_prices::post)
+        .service(import_etf_prices::post);
 }
