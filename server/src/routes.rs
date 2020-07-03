fc52b2b10c5d9b9f9c001cac748969b4fa1f02fd
lsunsi <sunsi.lucas@gmail.com>
2020-07-03 11:06:18 -0300

    Add import-trades endpoint that parses and creates the portfolio
    The endpoint follow the other ones in structure: gets file, parses
    and writes it. Nothing new here beyond the fact that this endpoint
    would actually be used by the user instead of me.

--- server/src/routes.rs
+++ server/src/routes.rs
@@ -1,11 +1,13 @@
 mod healthz;
 mod import_etf_prices;
+mod import_trades;
 mod import_treasury_prices;
 
 use actix_web::web::ServiceConfig;
 
 pub fn config(cfg: &mut ServiceConfig) {
     cfg.service(healthz::healthz)
-        .service(import_treasury_prices::post)
-        .service(import_etf_prices::post);
+        .service(import_trades::post)
+        .service(import_etf_prices::post)
+        .service(import_treasury_prices::post);
 }
