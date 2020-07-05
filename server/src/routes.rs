607a6f72ac609f4ab8bf830f5ca39d87a7652969
lsunsi <sunsi.lucas@gmail.com>
2020-07-05 18:16:12 -0300

    Add /portfolio-position GET route
    Just passing through the data from the models method. It'll let us
    kickstart the front-end. Let's see!

--- server/src/routes.rs
+++ server/src/routes.rs
@@ -2,6 +2,7 @@ mod healthz;
 mod import_etf_prices;
 mod import_trades;
 mod import_treasury_prices;
+mod portfolio_position;
 
 use actix_web::web::ServiceConfig;
 
@@ -9,5 +10,6 @@ pub fn config(cfg: &mut ServiceConfig) {
     cfg.service(healthz::healthz)
         .service(import_trades::post)
         .service(import_etf_prices::post)
-        .service(import_treasury_prices::post);
+        .service(import_treasury_prices::post)
+        .service(portfolio_position::get);
 }
