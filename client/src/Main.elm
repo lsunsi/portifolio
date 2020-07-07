135e521735303558eda8cac35495b127fd553eb6
lsunsi <sunsi.lucas@gmail.com>
2020-07-07 07:07:23 -0300

    Add basic structure and styling to the app
    It doesn't do anything yet, I just wanted to put what was in my mind
    into the code so I can focus in the functionality.

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,21 +1,61 @@
-module Main exposing (..)
+module Main exposing (main)
 
-import Browser
-import Html exposing (Html, div, h1, img, text)
-import Html.Attributes exposing (src)
+import Browser exposing (Document, UrlRequest(..))
+import Browser.Navigation exposing (Key, pushUrl)
+import Html exposing (Html, a, div, h1, img, span, text)
+import Html.Attributes exposing (class, classList, href)
+import Url exposing (Url)
+import Url.Parser as UrlParser
+
+
+urlToPage : Url -> Page
+urlToPage url =
+    let
+        pageParser =
+            UrlParser.oneOf
+                [ UrlParser.map Home UrlParser.top
+                , UrlParser.map Import (UrlParser.s "importar")
+                , UrlParser.map Position (UrlParser.s "posicao")
+                ]
+    in
+    UrlParser.parse pageParser url
+        |> Maybe.withDefault Unknown
+
+
+pageToPath : Page -> String
+pageToPath page =
+    case page of
+        Home ->
+            "/"
+
+        Import ->
+            "/importar"
+
+        Position ->
+            "/posicao"
+
+        Unknown ->
+            "/:eyes:"
 
 
 
 ---- MODEL ----
 
 
+type Page
+    = Home
+    | Import
+    | Position
+    | Unknown
+
+
 type alias Model =
-    {}
+    { key : Key, page : Page }
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( {}, Cmd.none )
+init : () -> Url -> Key -> ( Model, Cmd Msg )
+init () url key =
+    ( { key = key, page = urlToPage url }, Cmd.none )
 
 
 
@@ -23,34 +63,121 @@ init =
 
 
 type Msg
-    = NoOp
+    = UrlRequested UrlRequest
+    | UrlChanged Url
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        UrlRequested (Internal url) ->
+            ( model, pushUrl model.key (Url.toString url) )
+
+        UrlRequested (External _) ->
+            ( model, Cmd.none )
+
+        UrlChanged url ->
+            ( { model | page = urlToPage url }, Cmd.none )
 
 
 
 ---- VIEW ----
 
 
-view : Model -> Html Msg
-view model =
-    div []
-        [ h1 [] [ text "Your Elm App is working!" ]
+navbar : Page -> Html Msg
+navbar page =
+    let
+        navLinkOptions =
+            [ ( "Importar", Import )
+            , ( "Posição", Position )
+            ]
+
+        navLink ( label, navPage ) =
+            a
+                [ class "py-3"
+                , classList [ ( "font-semibold", page == navPage ) ]
+                , href (pageToPath navPage)
+                ]
+                [ text label ]
+    in
+    div [ class "border-r w-40 px-2" ]
+        [ a [ class "text-3xl font-medium h-16 text-center items-center flex", href (pageToPath Home) ]
+            [ span [ class "text-indigo-600" ] [ text "Porti" ]
+            , span [] [ text "folio" ]
+            ]
+        , div [ class "mt-6 flex flex-col font-light" ]
+            (List.map navLink navLinkOptions)
         ]
+
+
+header : Page -> Html Msg
+header page =
+    let
+        pageTitle =
+            case page of
+                Home ->
+                    Nothing
+
+                Import ->
+                    Just "Importar"
+
+                Position ->
+                    Just "Posição"
+
+                Unknown ->
+                    Nothing
+
+        titleHeader title =
+            [ div [ class "pl-4 text-2xl font-semibold border-l-2 border-indigo-600" ] [ text title ] ]
+    in
+    div
+        [ class "border-b h-16 flex items-center" ]
+        (pageTitle
+            |> Maybe.map titleHeader
+            |> Maybe.withDefault []
+        )
+
+
+content : Html Msg
+content =
+    div [ class "h-full" ] []
+
+
+body : Model -> List (Html Msg)
+body model =
+    [ div [ class "font-sans h-full" ]
+        [ div [ class "flex h-full" ]
+            [ navbar model.page
+            , div [ class "w-full" ]
+                [ header model.page, content ]
+            ]
+        ]
+    ]
+
+
+view : Model -> Document Msg
+view model =
+    { title = "📒 📈 Portifolio"
+    , body = body model
+    }
 
 
 
 ---- PROGRAM ----
 
 
+subs : Model -> Sub Msg
+subs _ =
+    Sub.none
+
+
 main : Program () Model Msg
 main =
-    Browser.element
+    Browser.application
         { view = view
-        , init = \_ -> init
+        , init = init
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = subs
+        , onUrlRequest = UrlRequested
+        , onUrlChange = UrlChanged
         }
