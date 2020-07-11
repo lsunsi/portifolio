d920628abbaf908b186ad5793d7266580984d0e9
lsunsi <sunsi.lucas@gmail.com>
2020-07-11 17:46:14 -0300

    Update import etfs route to support new service
    I mean, of course it looks way cleaner, the code was copied and hidden
    away. I guess it makes the code better. Or does it?

---
+++ server/src/routes/import_etfs_prices.rs
@@ -0,0 +1,28 @@
+use crate::database::Database;
+use crate::services::import_etfs_prices;
+use actix_web::{web::Data, HttpResponse};
+
+#[actix_web::post("/import-etfs-prices")]
+pub async fn post(db: Data<Database>) -> HttpResponse {
+    let conn = db.get().unwrap();
+
+    let results = import_etfs_prices::run(&conn)
+        .await
+        .into_iter()
+        .map(|(ticker, result)| {
+            (
+                ticker,
+                match result {
+                    Ok(()) => Ok(()),
+                    Err(import_etfs_prices::Error::Network(_)) => Err("Network"),
+                    Err(import_etfs_prices::Error::Status(_)) => Err("Status"),
+                    Err(import_etfs_prices::Error::Payload(_)) => Err("Payload"),
+                    Err(import_etfs_prices::Error::Parsing(_)) => Err("Parsing"),
+                    Err(import_etfs_prices::Error::Writing(_)) => Err("Writing"),
+                },
+            )
+        })
+        .collect::<Vec<_>>();
+
+    HttpResponse::Ok().json(results)
+}
