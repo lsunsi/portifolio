b9cf7fa3833590c9e44a1d00ea061aee054a1753
lsunsi <sunsi.lucas@gmail.com>
2020-07-12 07:59:00 -0300

    Add IVVB11 and SMAL11 to the ETFs the prices importer cares
    I mean, it's just a bunch of hardcode but it'll work. This endpoint
    only takes like a minute to respond. :')

--- server/src/services/import_etfs_prices.rs
+++ server/src/services/import_etfs_prices.rs
@@ -1,7 +1,6 @@
 use crate::models::register_etf_prices;
 use actix_web::client::Client;
 use bigdecimal::BigDecimal;
-use bytes::Bytes;
 use chrono::NaiveDate;
 use diesel::PgConnection;
 use scraper::{Html, Selector};
@@ -15,17 +14,37 @@ pub enum Error {
     Parsing(String),
 }
 
-async fn fetch() -> Result<Bytes, Error> {
+#[allow(non_snake_case)]
+#[derive(serde::Serialize)]
+struct FormParams {
+    curr_id: &'static str,
+    smlID: &'static str,
+    header: &'static str,
+    st_date: &'static str,
+    end_date: &'static str,
+    interval_sec: &'static str,
+    sort_col: &'static str,
+    sort_ord: &'static str,
+    action: &'static str,
+}
+
+async fn fetch(form_params: &FormParams) -> Result<bytes::Bytes, Error> {
     let mut response = Client::default()
         .post("https://br.investing.com/instruments/HistoricalDataAjax")
+        .header("Accept", "text/plain, */*; q=0.01")
+        .header("Accept-Language", "en-US,en;q=0.5")
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .header("X-Requested-With", "XMLHttpRequest")
+        .header("Origin", "https://www.investing.com")
+        .header("DNT", "1")
+        .header("Connection", "keep-alive")
         .header(
             "User-Agent",
-            "Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0",
+            "Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
         )
-        .header("X-Requested-With", "XMLHttpRequest")
-        .header("Content-Type", "application/x-www-form-urlencoded")
-        .send_body("curr_id=39004&smlID=2514218&header=BOVA11+-+Dados+Hist%C3%B3ricos&st_date=28%2F05%2F2000&end_date=28%2F06%2F2020&interval_sec=Daily&sort_col=date&sort_ord=DESC&action=historical_data")
-        .await.map_err(Error::Network)?;
+        .send_form(form_params)
+        .await
+        .map_err(Error::Network)?;
 
     let status = response.status();
     if status != 200 {
@@ -39,7 +58,7 @@ async fn fetch() -> Result<Bytes, Error> {
         .map_err(Error::Payload)
 }
 
-fn parse(bytes: Bytes) -> Result<Vec<(NaiveDate, BigDecimal)>, Error> {
+fn parse(bytes: bytes::Bytes) -> Result<Vec<(NaiveDate, BigDecimal)>, Error> {
     let string = std::str::from_utf8(&bytes)
         .map_err(|_| Error::Parsing("Body is not a utf8 string".into()))?;
 
@@ -101,14 +120,60 @@ fn write(
 }
 
 pub async fn run(conn: &PgConnection) -> Vec<(&'static str, Result<(), Error>)> {
-    let ticker = "BOVA11";
+    let tickers_params = [
+        ("BOVA11", BOVA11_FORM_PARAMS),
+        ("SMAL11", SMAL11_FORM_PARAMS),
+        ("IVVB11", IVVB11_FORM_PARAMS),
+    ];
 
-    let res = fetch()
-        .await
-        .and_then(parse)
-        .and_then(|lines| write(conn, ticker, lines));
+    let mut results = vec![];
 
-    vec![(ticker, res)]
+    for (ticker, params) in tickers_params.iter() {
+        let result = fetch(params)
+            .await
+            .and_then(parse)
+            .and_then(|lines| write(conn, ticker, lines));
+
+        results.push((*ticker, result));
+    }
+
+    results
 }
 
 const PAYLOAD_LIMIT: usize = 16777216;
+
+const BOVA11_FORM_PARAMS: FormParams = FormParams {
+    curr_id: "39004",
+    smlID: "2514218",
+    header: "BOVA11+Historical+Data",
+    st_date: "01/01/2000",
+    end_date: "01/01/2022",
+    interval_sec: "Daily",
+    sort_col: "date",
+    sort_ord: "DESC",
+    action: "historical_data",
+};
+
+const SMAL11_FORM_PARAMS: FormParams = FormParams {
+    curr_id: "39013",
+    smlID: "2514317",
+    header: "SMAL11+Historical+Data",
+    st_date: "01/01/2000",
+    end_date: "01/01/2022",
+    interval_sec: "Daily",
+    sort_col: "date",
+    sort_ord: "DESC",
+    action: "historical_data",
+};
+
+const IVVB11_FORM_PARAMS: FormParams = FormParams {
+    curr_id: "956435",
+    smlID: "2585374",
+    header: "IVVB11+Historical+Data",
+    st_date: "01/01/2000",
+    end_date: "01/01/2022",
+    interval_sec: "Daily",
+    sort_col: "date",
+    sort_ord: "DESC",
+    action: "historical_data",
+};
