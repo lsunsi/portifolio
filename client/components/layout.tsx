12726581a10ad53d396f6f94d2e208721b18d5b4
lsunsi <sunsi.lucas@gmail.com>
2020-07-18 11:54:37 -0300

    Add basic navigation to the app layout
    This is basically just to help me navigate without
    typing all the routes by hand. I guess it also works
    for the user, haha. A more complete solution is required
    for the future.

--- client/components/layout.tsx
+++ client/components/layout.tsx
@@ -1,29 +1,55 @@
 import Base from "./base";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 type LayoutProps = {
   title: string;
   children: React.ReactNode;
 };
 
-const Layout = ({ title, children }: LayoutProps) => (
-  <Base title={title}>
-    <div className="uk-flex uk-flex-right">
-      <div className="uk-flex-1">
-        <h1 className="uk-heading-line">
-          <span>{title}</span>
-        </h1>
-        {children}
-      </div>
+const Layout = ({ title, children }: LayoutProps) => {
+  const { pathname } = useRouter();
 
-      <div className="uk-flex-none">
-        <h1 className="uk-margin-left">
-          <span>
-            Porti<span className="uk-text-primary">folio</span>
-          </span>
-        </h1>
+  return (
+    <Base title={title}>
+      <div className="uk-flex uk-flex-right">
+        <div className="uk-flex-1">
+          <h1 className="uk-heading-line">
+            <span>{title}</span>
+          </h1>
+          {children}
+        </div>
+
+        <div className="uk-flex-none">
+          <h1 className="uk-margin-left">
+            <Link href="/">
+              <span>
+                Porti<span className="uk-text-primary">folio</span>
+              </span>
+            </Link>
+          </h1>
+
+          <ul className="uk-nav uk-nav-primary uk-text-right">
+            <li className={`${pathname == "/position" ? "uk-active" : ""}`}>
+              <Link href="/position">
+                <a>Posição</a>
+              </Link>
+            </li>
+            <li className={`${pathname == "/history" ? "uk-active" : ""}`}>
+              <Link href="/history">
+                <a>Histórico</a>
+              </Link>
+            </li>
+            <li className={`${pathname == "/transactions" ? "uk-active" : ""}`}>
+              <Link href="/transactions">
+                <a>Transações</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </Base>
-);
+    </Base>
+  );
+};
 
 export default Layout;
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,13 +1,51 @@
 import Base from "components/base";
+import Link from "next/link";
+import { GetServerSideProps } from "next";
 
-const Index = () => (
+type Props = {
+  hasCookie: boolean;
+};
+
+const Index = ({ hasCookie }: Props) => (
   <Base title="Olá!">
     <h1 className="uk-heading-medium uk-heading-line uk-text-center">
       <span>
         Porti<span className="uk-text-primary">folio</span>
       </span>
     </h1>
+
+    <ul className="uk-nav uk-nav-primary uk-text-center">
+      {hasCookie ? (
+        <>
+          <li>
+            <Link href="/position">
+              <a>Posição</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/history">
+              <a>Histórico</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/transactions">
+              <a>Transações</a>
+            </Link>
+          </li>
+        </>
+      ) : (
+        <li>
+          <Link href="/import">
+            <a>Importar</a>
+          </Link>
+        </li>
+      )}
+    </ul>
   </Base>
 );
+
+export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => ({
+  props: { hasCookie: !!ctx.req.headers.cookie },
+});
 
 export default Index;
