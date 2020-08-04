38e3f58fbfea53332613a8c329b5f116740e3f7f
lsunsi <sunsi.lucas@gmail.com>
2020-08-04 07:42:03 -0300

    Add basic logging setup for actix
    Really, I was creeped out with the silence in the server running.
    Now we're seeing at least the request logs. Much better.

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,18 +7,23 @@ mod schema;
 mod services;
 mod web;
 
+use actix_cors::Cors;
+use actix_web::{middleware::Logger, App, HttpServer};
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     let database = database::init();
+    env_logger::init();
 
-    actix_web::HttpServer::new(move || {
-        actix_web::App::new()
+    HttpServer::new(move || {
+        App::new()
             .wrap(
-                actix_cors::Cors::new()
+                Cors::new()
                     .supports_credentials()
                     .allowed_origin("http://localhost:3000")
                     .finish(),
             )
+            .wrap(Logger::default())
             .data(database.clone())
             .configure(web::routes::config)
     })
